d62b218dbf3beb919ce54feb3d17f0fe9d4befe2
Sema Gerger <57225099+SemaGerger@users.noreply.github.com>
2026-06-09 10:50:20 +0300

    first comment

---
+++ config.js
@@ -0,0 +1,6 @@
+// Google Sheet ayarlarını buraya ekle.
+// Bu dosyayı değiştirerek siteyi yeniden yönlendirebilirsin.
+window.siteConfig = {
+  googleSheetId: '1SI2TyuLLDeYIGpQN8cmeUBBtXszPQXzpiu4GpKPXu1A',
+  googleSheetTab: ''
+};
